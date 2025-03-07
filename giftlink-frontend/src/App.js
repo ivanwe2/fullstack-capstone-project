128e9185fd74a53a8d9448a8db890df7e3cb5f1f
ivanwe <rusev37@gmail.com>
2025-03-07 10:07:35 -0500

    fix warnings

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -7,9 +7,10 @@ import Navbar from './components/Navbar/Navbar';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import Profile from './components/Profile/Profile';
+import SearchPage from './components/SearchPage/SearchPage';
+import DetailsPage from './components/DetailsPage/DetailsPage';
 
 function App() {
-const navigate = useNavigate();
   return (
     <>
         <Navbar/>
@@ -20,6 +21,8 @@ const navigate = useNavigate();
           <Route path="/app/login" element={<LoginPage/>} />
           <Route path="/app/register" element={<RegisterPage />} />
           <Route path="/app/profile" element={<Profile />} />
+          <Route path="/app/search" element={<SearchPage/>} />
+          <Route path="/app/product/:productId" element={<DetailsPage/>} />
         </Routes>
         </>
   );
